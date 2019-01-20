74d80a3524eb4b2add3d4c0ae2658026c1b0704e
Romario Williams <romariowilliams@hotmail.com>
2019-01-20 02:48:44 -0500

    Update index.html, main.css, todo.js

--- todo.js
+++ todo.js
@@ -14,14 +14,14 @@ function addToList(task){
   if(htmlstring == "<br><br>Add a task to begin.") {  document.getElementById("list-container").innerHTML ='';}
 
   todoList.push(task);
-  var taskID= todoList.lenght-1;
+  var taskID= todoList.length-1;
   createDiv(task,taskID);
   saveTodoList();
 }
 
 function createDiv(task,taskID){
    var todoContainer = document.createElement('div');
-   todoContainer.id="task";
+   todoContainer.className="task";
    var todoElement = document.createElement('div');
    todoElement.className = 'todo-element';
    todoElement.id = "task"+taskID;
@@ -36,8 +36,6 @@ function createDiv(task,taskID){
    todoContainer.setAttribute('onclick','callTask(lastChild.id);');
    addDivToList(todoContainer);
 }
-
-
 
 function addDivToList(todoContainer){
       document.getElementById("list-container").prepend(todoContainer);
@@ -57,28 +55,25 @@ function getTodoList(){
 }
 
 function displaySavedList(){
-  getTodoList();
-    document.getElementById("list-container").innerHTML='';
-    for (i=0; i< todoList.length; i++){
-    createDiv(todoList[i],i);
+    getTodoList();
+    if (todoList.length==0) {
+      displayEmptyMsg();
     }
-    displayEmptyMsg();
-  }
+    else {
+      document.getElementById("list-container").innerHTML='';
+      for (i=0; i< todoList.length; i++){
+        createDiv(todoList[i],i);
+      }
+    }
 
-  function callTask(taskIndex) {
+}
+
+function callTask(taskIndex) {
     if (VERBOSE) alert (taskIndex);
-  }
-/*
-  function strikeTask(taskIndex){
-    var currentHTML;
-    currentHTML = document.getElementById("task"+taskIndex).innerHTML;
-    document.getElementById("task"+taskIndex).innerHTML= '<span style=color:grey;"><del>'+currentHTML+'</del></span>';
-  }
-*/
+}
+
 function displayEmptyMsg() {
-  if (todoList.length==0) {
      document.getElementById("list-container").innerHTML = '<br><br>Add a task to begin.';
-  }
 }
 
 function removeTask(taskIndex) {
@@ -88,5 +83,4 @@ function removeTask(taskIndex) {
   }
   saveTodoList();
   displaySavedList();
-
 }
